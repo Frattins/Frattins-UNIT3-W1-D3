fe799a01c3632178bb631b6d92996042014bba8f
Frattins <davide.frattini1@gmail.com>
2024-05-22 18:52:06 +0200

    prova

--- esercizioAngular/src/app/app.component.ts
+++ esercizioAngular/src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'esercizioAngular';
+export class AppComponent implements OnInit {
+  data: iPost[] = [];  // Usa l'interfaccia per definire il tipo di dati
+
+  constructor(private ActivePostComponent: ActivePostComponent) { }  // Inietta il servizio
+
+  ngOnInit(): void {
+    this.ActivePostComponent.getData().subscribe((data) => {
+      this.data = data;
+    });
+  }
 }
