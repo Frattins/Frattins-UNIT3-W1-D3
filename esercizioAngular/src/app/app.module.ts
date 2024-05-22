cdd96fb288d913a1d8a0e6c1bf672fff235a77c0
Frattins <davide.frattini1@gmail.com>
2024-05-22 16:16:34 +0200

    commit

--- esercizioAngular/src/app/app.module.ts
+++ esercizioAngular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivePostComponent } from './active-post/active-post.component';
+import { InactivePostComponent } from './inactive-post/inactive-post.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostdetailComponent } from './postdetail/postdetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivePostComponent,
+    InactivePostComponent,
+    HomeComponent,
+    NavbarComponent,
+    PostdetailComponent
   ],
   imports: [
     BrowserModule,
